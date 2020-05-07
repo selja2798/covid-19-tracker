56f5d3f9f17e9133ae1d2dc6294e431a91de74fb
selja2798 <siotong2798@gmail.com>
2020-05-07 17:14:54 +0700

    Updates

---
+++ precache-manifest.8ae510d8ade407a1adcf7348f73aec7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647eecf6ef30298c812ccd762a65a83a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "1f79bab5b7090128914e",
+    "url": "/covid-19-tracker/static/css/main.257c005d.chunk.css"
+  },
+  {
+    "revision": "0c0c00d4d06caa788a50",
+    "url": "/covid-19-tracker/static/js/2.7a4c73ef.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.7a4c73ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f79bab5b7090128914e",
+    "url": "/covid-19-tracker/static/js/main.6877ebc0.chunk.js"
+  },
+  {
+    "revision": "a2a7c00d66ff85ef819f",
+    "url": "/covid-19-tracker/static/js/runtime-main.f9cdd971.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
--- src/components/export.js
+++
@@ -1,3 +0,0 @@
-export { default as Cards } from "./Card/card";
-export { default as CountryPicker } from "./Country-picker/country-picker";
-export { default as Chart } from "./Chart/chart";
